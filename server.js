979d61bcc8ed009db85fe2c972a84c2f49f676cb
Balde-Baile <baldassarinigabriel@gmail.com>
2024-06-09 21:00:48 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let problemas = [];
+
+app.post('/api/problemas', (req, res) => {
+    const problema = req.body.problema;
+    if (problema) {
+        problemas.push(problema);
+        res.status(201).json({ message: 'Problema recebido com sucesso!' });
+    } else {
+        res.status(400).json({ message: 'Erro: Problema não enviado.' });
+    }
+});
+
+app.get('/api/problemas', (req, res) => {
+    res.status(200).json(problemas);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
